5e5826cabfc78328e334721763c1811f78f15026
m4r1no0 <marinoosorio97@gmail.com>
2026-03-27 08:12:14 -0500

    totalRegistros

--- app/router/instructor.py
+++ app/router/instructor.py
@@ -99,7 +99,7 @@ def listar_por_supervisor(
 @router.get("/")
 def listar_instructores(
     page: int = Query(1, ge=1),
-    size: int = Query(10, ge=1, le=100),
+    size: int = Query(10, ge=1, le=200),
     db: Session = Depends(get_db),
     current_user: dict = Depends(get_current_user)
 ):
